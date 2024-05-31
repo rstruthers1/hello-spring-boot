291c718461b377b127cd992067cac5289afb4de0
Rachel Struthers <rstruthers1@yahoo.com>
2024-05-31 13:49:48 -0500

    Security

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication {
 			System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
 			System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
 			System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+			System.setProperty("spring.security.user.password", dotenv.get("SPRING_SECURITY_USER_PASSWORD"));
 
 		}
 		SpringApplication.run(HelloApplication.class, args);
