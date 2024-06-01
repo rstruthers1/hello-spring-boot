5ab4836b16d8970139760390dedd8955a1a6c1ca
Rachel Struthers <rstruthers1@yahoo.com>
2024-06-01 12:10:55 -0500

    Hello AWS Secrets Manager

--- src/main/java/com/example/hello/controllers/HelloController.java
+++ src/main/java/com/example/hello/controllers/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
         @GetMapping("/hello")
         public String hello() {
-            return "Hello helm!!!!";
+            return "Hello AWS Secrets Manager!";
         }
 
 }
